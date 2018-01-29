74efd613dc4f4bf8c3cb7a84af39dac79bb434af
Mohamed Adel <mohamed.adel@eye-ltd.com>
2018-01-29 16:56:03 +0200

    Init Project

---
+++ Assets/_Scripts/AuthManager.cs
@@ -0,0 +1,83 @@
+﻿using System.Collections;
+using System.Threading;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class AuthManager : MonoBehaviour {
+
+    public InputField displayName, username, password;
+    public Button Loginbtn, Registerbtn;
+    public Text status;
+
+    public void Register(string displayName, string username, string password)
+    {
+        Debug.Log("Registering...");
+        new GameSparks.Api.Requests.RegistrationRequest()
+            .SetDisplayName(displayName)
+            .SetUserName(username)
+            .SetPassword(password)
+            .Send((response) =>
+            {
+                if (!response.HasErrors)
+                {
+                    Debug.Log(response.DisplayName + " registered successfully");
+                }
+                else
+                {
+                    Debug.LogError("couldn't register, Error: " + response.Errors);
+                }
+
+            });
+    }
+
+    public void Login(string username, string password)
+    {
+        Debug.Log("Login...");
+        new GameSparks.Api.Requests.AuthenticationRequest()
+                    .SetUserName(username)
+                    .SetPassword(password)
+                    .Send((response) =>
+                    {
+                        if (!response.HasErrors)
+                        {
+                            Debug.Log(response.DisplayName + " registered successfully");
+                        }
+                        else
+                        {
+                            Debug.LogError("couldn't register, Error: " + response.Errors);
+                        }
+
+                    });
+    }
+
+    public void LoginByDeviceInfo(string displayName)
+    {
+        Debug.Log("Device Login...");
+        new GameSparks.Api.Requests.DeviceAuthenticationRequest()
+            .SetDisplayName(displayName)
+            .Send((response) =>
+            {
+                if (!response.HasErrors)
+                {
+                    Debug.Log(response.DisplayName + " registered successfully");
+                }
+                else
+                {
+                    Debug.LogError("couldn't register, Error: " + response.Errors);
+                }
+
+            });
+    }
+
+    // Utilities
+    private void ToggleButtonStates(bool toState)
+    {
+        Registerbtn.interactable = toState;
+        Loginbtn.interactable = toState;
+    }
+
+    private void UpdateStatus(string message)
+    {
+        status.text = message;
+    }
+}
---
+++ Assets/_Scripts/RegisterPlayer.cs
@@ -0,0 +1,68 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class RegisterPlayer : MonoBehaviour
+{
+
+    public Text displayName, username, password;
+
+
+    public void Register()
+    {
+        Debug.Log("Registering...");
+        new GameSparks.Api.Requests.RegistrationRequest()
+            .SetDisplayName(displayName.text)
+            .SetUserName(username.text)
+            .SetPassword(password.text)
+            .Send((response) =>
+            {
+                if (!response.HasErrors)
+                {
+                    Debug.Log(response.DisplayName + " registered successfully");
+                }
+                else
+                {
+                    Debug.LogError("couldn't register, Error: " + response.Errors);
+                }
+            });
+    }
+
+    public void Login()
+    {
+        Debug.Log("Login...");
+        new GameSparks.Api.Requests.AuthenticationRequest()
+                    .SetUserName(username.text)
+                    .SetPassword(password.text)
+                    .Send((response) =>
+                    {
+                        if (!response.HasErrors)
+                        {
+                            Debug.Log(response.DisplayName + " registered successfully");
+                        }
+                        else
+                        {
+                            Debug.LogError("couldn't register, Error: " + response.Errors);
+                        }
+                    });
+    }
+
+    public void LoginByDeviceInfo()
+    {
+        Debug.Log("Device Login...");
+        new GameSparks.Api.Requests.DeviceAuthenticationRequest()
+            .SetDisplayName(displayName.text)
+            .Send((response) =>
+            {
+                if (!response.HasErrors)
+                {
+                    Debug.Log(response.DisplayName + " registered successfully");
+                }
+                else
+                {
+                    Debug.LogError("couldn't register, Error: " + response.Errors);
+                }
+            });
+    }
+}
